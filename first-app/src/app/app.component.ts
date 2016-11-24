f5fd003a5e637372ac1cfcb45f4dc803808925e3
Odiki <rodion.zinovyev@gmail.com>
2016-11-24 13:11:32 +0200

    New component

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>Inline Template</h1>
+  <fa-other></fa-other>
+  `,
+  styles: [`
+  h1{
+    color: red;
+  }
+
+  `]
 })
 export class AppComponent {
   title = "I've changed it!";
