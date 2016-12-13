b02e18de6699afcb7c0ee8b5f4ae7df0d0d34f02
Odiki <rodion.zinovyev@gmail.com>
2016-12-13 16:09:12 +0200

    Two wat bindings

---
+++ first-app/src/app/databinding/two-way-binding.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-two-way-binding',
+  template: `<input type = "text">`
+  
+  ,
+  styles: []
+})
+export class TwoWayBindingComponent  {
+
+
+}
